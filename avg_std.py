97ee7b0c8d71c23434657516cc4861cafe41fb40
PrekshaNema25 <cs15d201@smail.iitm.ac.in>
2018-08-22 09:53:42 +0200

    Update avg_std.py

--- avg_std.py
+++ avg_std.py
@@ -24,32 +24,28 @@ print ((np.std(ner) + np.std(qt) + np.std(sw) +  np.std(re) + np.std(d))/5)
 
 all_scores = pickle.load(open(sys.argv[2],"rb"))
 
-print(len(all_scores))
-ref_scores = [x['ref'] for x in all_scores]
-fluent_scores = [x[sys.argv[3]] for x in all_scores]
-imp_scores =  [x['imp'] for x in all_scores]
-qt_scores = [x['qt'] for x in all_scores]
-sw_scores = [x['sw'] for x in all_scores]
-ner_scores =  [x['ner'] for x in all_scores]
+def get_answerability_score(all_scores, ner_weight, qt_weight, re_weight, d, ngram_metric="Bleu_4"):
+	print(len(all_scores))
+	ref_scores = [x['ref'] for x in all_scores]
+	fluent_scores = [x[ngram_metric] for x in all_scores]
+	imp_scores =  [x['imp'] for x in all_scores]
+	qt_scores = [x['qt'] for x in all_scores]
+	sw_scores = [x['sw'] for x in all_scores]
+	ner_scores =  [x['ner'] for x in all_scores]
 
+	new_scores = []
 
-ner_weight, qt_weight, re_weight, d = np.mean(ner), np.mean(qt), np.mean(re), np.mean(d)
+	for i in range(len(imp_scores)):
+	    answerability = re_weight*imp_scores[i] + ner_weight*ner_scores[i]  + \
+		    qt_weight*qt_scores[i] + (1-re_weight - ner_weight - qt_weight)*sw_scores[i]
 
-new_scores = []
+	    temp = d*answerability + (1-d)*fluent_scores[i]
+	    new_scores.append(temp)
+	    print ("New Score:{} Ner Score: {} RE Score {} SW Score {} QT Score {} ".format(temp, ner_scores[i], imp_scores[i], sw_scores[i], qt_scores[i]))
 
-for i in range(len(imp_scores)):
-    answerability = re_weight*imp_scores[i] + ner_weight*ner_scores[i]  + \
-            qt_weight*qt_scores[i] + (1-re_weight - ner_weight - qt_weight)*sw_scores[i]
-
-    temp = d*answerability + (1-d)*fluent_scores[i]
-    new_scores.append(temp)
-    #print ("True : ", true[i])
-    #print ("Pred : " , pred[i])
-    #print ("New Score:{} Ner Score: {} RE Score {} SW Score {} QT Score {} ".format(temp, ner_scores[i], imp_scores[i], sw_scores[i], qt_scores[i]))
-
-print (np.mean(fluent_scores), np.mean(new_scores))
-np.savetxt('refscores', fluent_scores)
-np.savetxt('goldscores',new_scores)
+	print ("Mean Answerability Score Across Questions: {} N-gram Score: {}".format(np.mean(fluent_scores), np.mean(new_scores)))
+	np.savetxt(os.path.join(args.output_dir , 'ngram_scores.txt'), fluent_scores)
+	np.savetxt(os.path.join(args.output_dir , 'answerability_scores.txt'),new_scores)
 """
 a = ref_scores
 #noise = np.random.normal(0,0.03, len(ref_scores))
